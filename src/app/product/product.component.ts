e97046929260bdd7ccaebb7ff71ba79c6d82a42f
Mujib517 <mujib517@gmail.com>
2018-01-11 21:39:37 +0530

    Moment

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from "@angular/core";
+import * as moment from 'moment';
 
 @Component({
     selector: 'app-product',
     template: ` <h3>{{product.brand | uppercase}}</h3>
     <div>{{product.model | lowercase}}</div>
     <div>{{product.price | currency}}</div>
-    <div>{{product.lastUpdated | date:'MM-dd-yyyy hh:mm:ss a'}}</div>`
+    <div>{{product.lastUpdated | date:'MM-dd-yyyy hh:mm:ss a'}}</div>
+    <div>{{getRelativeTime(product.lastUpdated)}}</div>
+    `
 })
 export class ProductComponent {
 
@@ -13,5 +16,9 @@ export class ProductComponent {
     product: any;
 
     constructor() {
+    }
+
+    getRelativeTime(date): string {
+        return moment(date).fromNow();
     }
 }
