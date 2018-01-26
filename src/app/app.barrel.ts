87edac2ec98e6cf4bc90a3775fc35269401e9cc7
Mujib517 <mujib517@gmail.com>
2018-01-26 21:33:07 +0530

    Barrels

---
+++ src/app/app.barrel.ts
@@ -0,0 +1,14 @@
+export { AppComponent } from './app.component';
+export { HomeComponent } from "./home/home.component";
+export { ProductListComponent } from "./product-list/product-list.component";
+export { ProductComponent } from "./product/product.component";
+export { UsersComponent } from "./users/users.component";
+export { ProductService } from "./shared/product.service";
+export { HeaderComponent } from './header/header.component';
+export { FooterComponent } from './footer/footer.component';
+export { AboutComponent } from './about/about.component';
+export { ContactComponent } from './contact/contact.component';
+export { ProductDetailComponent } from './product-detail/product-detail.component';
+export { SpecsComponent } from './specs/specs.component';
+export { ReviewsComponent } from './reviews/reviews.component';
+export { NewProductComponent } from './new-product/new-product.component';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,13 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
-import { HomeComponent } from "./home/home.component";
-import { ProductListComponent } from "./product-list/product-list.component";
-import { ProductComponent } from "./product/product.component";
-import { UsersComponent } from "./users/users.component";
-import { ProductService } from "./shared/product.service";
 import { SharedModule } from "./shared/shared.module";
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
 import { AppRouterModule } from "./shared/app-router.module";
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { SpecsComponent } from './specs/specs.component';
-import { ReviewsComponent } from './reviews/reviews.component';
-import { NewProductComponent } from './new-product/new-product.component';
+
+import { AppComponent, HomeComponent, ProductListComponent, 
+        ProductComponent, UsersComponent, HeaderComponent, FooterComponent, 
+        AboutComponent, ContactComponent, ProductDetailComponent, SpecsComponent, 
+        ReviewsComponent, NewProductComponent } from './app.barrel';
 
 
 @NgModule({
