511e52bb870ce1261810078032c25a3298a4fd72
Mujib517 <mujib517@gmail.com>
2018-01-26 21:38:28 +0530

    Environments

--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -2,27 +2,29 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
 import { Product } from "./models/product.model";
+import { environment } from "../../environments/environment";
 
 @Injectable()
 export class ProductService {
 
     reviews: any[];
+    url: string = environment.url;
 
     constructor(private http: HttpClient) { }
 
     get(): Observable<Product[]> {
-        return this.http.get<Product[]>("https://exp-rest-api.herokuapp.com/api/products");
+        return this.http.get<Product[]>(this.url);
     }
 
     getById(id: string): Observable<any> {
-        return this.http.get("https://exp-rest-api.herokuapp.com/api/products/" + id);
+        return this.http.get(this.url + id);
     }
 
     save(product: any): Observable<any> {
-        return this.http.post("https://exp-rest-api.herokuapp.com/api/products", product);
+        return this.http.post(this.url, product);
     }
 
     delete(id: string): Observable<any> {
-        return this.http.delete("https://exp-rest-api.herokuapp.com/api/products/" + id);
+        return this.http.delete(this.url + id);
     }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  url: "https://exp-rest-api.herokuapp.com/api/products/"
 };
